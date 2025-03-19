689c11a0563864d88def5520c39c47506986f9c2
Pablo Menichelli <meni.pablo@gmail.com>
2025-03-19 19:18:55 +0100

    update

--- content_script.js
+++ content_script.js
@@ -1,6 +1,6 @@
 browser.runtime.onMessage.addListener(message => {
     if (message.type === "interaction") {
-        console.log('content_script received', message.action, message.args, message.vPointerCoords);
+        console.log('EXTENSION: content_script received', message.action, message.args);
         const args = message.args ?? [];
         browser.virtualSession.interaction[message.action](...args);
     } else if (message.type === "run_actions") {
@@ -21,8 +21,104 @@ runActions = async interactions => {
         const interactionName = mapping[action] ?? action;
 
         const args = Object.values(interaction);
+        console.log('EXTENSION: running', interactionName, args);
         await browser.virtualSession.interaction[interactionName](...args);
     }
 };
 
-console.log('content_script loaded');
+document.addEventListener('DOMContentLoaded', async () => {
+    const inputBounds = document.querySelector('#kiwi').getBoundingClientRect();
+    const inputCenterX = inputBounds.left + inputBounds.width / 2;
+    const inputCenterY = inputBounds.top + inputBounds.height / 2;
+
+    const imgBounds = document.querySelector('img').getBoundingClientRect();
+    const imgX = imgBounds.left + 1;
+    const imgY = imgBounds.top + 1;
+
+    const INTERACTIONS = [
+        {
+            "action": "mouse_move",
+            "coordinate": [inputCenterX, inputCenterY]
+        },
+        {
+            "action": "wait",
+            "duration": 1000
+        },
+        {
+            "action": "left_click",
+        },
+        {
+            "action": "wait",
+            "duration": 1000
+        },
+        {
+            "action": "type",
+            "text": "The work is mysterious and important...!"
+        },
+        {
+            "action": "wait",
+            "duration": 1000,
+        },
+        {
+            "action": "mouse_move",
+            "coordinate": [imgX, imgY]
+        },
+        {
+            "action": "wait",
+            "duration": 1000
+        },
+        {
+            "action": "scroll",
+            "amount": 300,
+            "direction": "down"
+        },
+        {
+            "action": "wait",
+            "duration": 1000,
+        },
+        {
+            "action": "scroll",
+            "amount": 300,
+            "direction": "down"
+        },
+        {
+            "action": "wait",
+            "duration": 1000,
+        },
+        {
+            "action": "scroll",
+            "amount": 100,
+            "direction": "down"
+        },
+        {
+            "action": "wait",
+            "duration": 1000,
+        },
+        {
+            "action": "scroll",
+            "amount": 100,
+            "direction": "down"
+        },
+        {
+            "action": "mouse_move",
+            "coordinate": [300, 700]
+        },
+        {
+            "action": "left_click",
+        },
+        {
+            "action": "scroll",
+            "amount": 300,
+            "direction": "down"
+        },
+        {
+            "action": "wait",
+            "duration": 1000,
+        },
+    ];
+    const btn = document.getElementById('run-actions');
+    btn.addEventListener('click', async () => {
+        await runActions(INTERACTIONS);
+    });
+    console.log('EXTENSION: content_script loaded');
+});

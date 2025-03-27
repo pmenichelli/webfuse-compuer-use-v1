bab7640c3a468b41732762e2a5a4057252631363
Pablo Menichelli <meni.pablo@gmail.com>
2025-03-27 14:04:01 +0100

    improve extension

--- content_script.js
+++ content_script.js
@@ -13,21 +13,24 @@ const mapping = {
     left_click: 'click',
 }
 
-runActions = async interactions => {
-    for (let interaction of interactions) {
-        const { action } = interaction;
-        delete interaction.action;
+const runActions = async automations => {
+    for (const automation of automations) {
+        const { action } = automation;
+        delete automation.action;
 
-        const interactionName = mapping[action] ?? action;
+        const automationName = mapping[action] ?? action;
 
-        const args = Object.values(interaction);
-        console.log('EXTENSION: running', interactionName, args);
-        await browser.virtualSession.interaction[interactionName](...args);
+        const args = Object.values(automation);
+        console.log('EXTENSION: running', automationName, ...args);
+        await browser.virtualSession.automation[automationName](...args);
     }
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const inputBounds = document.querySelector('#kiwi').getBoundingClientRect();
+    if (!location.href.includes('test_extension_automations.html')){
+        return;
+    }
+    const inputBounds = document.querySelector('#input-element').getBoundingClientRect();
     const inputCenterX = inputBounds.left + inputBounds.width / 2;
     const inputCenterY = inputBounds.top + inputBounds.height / 2;
 
@@ -35,90 +38,39 @@ document.addEventListener('DOMContentLoaded', async () => {
     const imgX = imgBounds.left + 1;
     const imgY = imgBounds.top + 1;
 
-    const INTERACTIONS = [
-        {
-            "action": "mouse_move",
-            "coordinate": [inputCenterX, inputCenterY]
-        },
-        {
-            "action": "wait",
-            "duration": 1000
-        },
-        {
-            "action": "left_click",
-        },
-        {
-            "action": "wait",
-            "duration": 1000
-        },
-        {
-            "action": "type",
-            "text": "The work is mysterious and important...!"
-        },
-        {
-            "action": "wait",
-            "duration": 1000,
-        },
-        {
-            "action": "mouse_move",
-            "coordinate": [imgX, imgY]
-        },
-        {
-            "action": "wait",
-            "duration": 1000
-        },
-        {
-            "action": "scroll",
-            "amount": 300,
-            "direction": "down"
-        },
-        {
-            "action": "wait",
-            "duration": 1000,
-        },
-        {
-            "action": "scroll",
-            "amount": 300,
-            "direction": "down"
-        },
-        {
-            "action": "wait",
-            "duration": 1000,
-        },
-        {
-            "action": "scroll",
-            "amount": 100,
-            "direction": "down"
-        },
-        {
-            "action": "wait",
-            "duration": 1000,
-        },
-        {
-            "action": "scroll",
-            "amount": 100,
-            "direction": "down"
-        },
-        {
-            "action": "mouse_move",
-            "coordinate": [300, 700]
-        },
-        {
-            "action": "left_click",
-        },
-        {
-            "action": "scroll",
-            "amount": 300,
-            "direction": "down"
-        },
-        {
-            "action": "wait",
-            "duration": 1000,
-        },
+    const AUTOMATIONS = [
+        // click and type
+        { "action": "mouse_move", "coordinate": [inputCenterX, inputCenterY] },
+        { "action": "wait", "duration": 1000 },
+        { "action": "left_click" },
+        { "action": "wait", "duration": 1000 },
+        { "action": "type", "text": "The work is mysterious and important...!" },
+        { "action": "wait", "duration": 1000 },
+
+        // scrolling
+        { "action": "mouse_move", "coordinate": [imgX, imgY] },
+        { "action": "wait", "duration": 1000 },
+        { "action": "scroll", "amount": 300, "direction": "down" },
+        { "action": "wait", "duration": 1000 },
+        { "action": "scroll", "amount": 800, "direction": "down" },
+        { "action": "wait", "duration": 1000 },
+        { "action": "scroll", "amount": 400, "direction": "down" },
+
+        // more scrolling
+        { "action": "wait", "duration": 1000 },
+        { "action": "scroll", "amount": 100, "direction": "down" },
+        { "action": "wait", "duration": 1000 },
+        { "action": "scroll", "amount": 100, "direction": "down" },
+
+        // click on the main page and scroll
+        { "action": "mouse_move", "coordinate": [300, 700] },
+        { "action": "left_click" },
+        { "action": "scroll", "amount": 600, "direction": "up" },
+        { "action": "wait", "duration": 1000 },
     ];
     const btn = document.getElementById('run-actions');
     btn.addEventListener('click', async () => {
-        await runActions(INTERACTIONS);
+        await runActions(AUTOMATIONS);
     });
     console.log('EXTENSION: content_script loaded');
 });
